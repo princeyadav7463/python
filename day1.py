7188e263e85e21273f2f993f98961280046a0621
princeyadav7463 <143525918+princeyadav7463@users.noreply.github.com>
2025-06-12 18:29:29 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,14 @@
+# day  1
+
+print("Hello World")
+print(5)
+print("Bye")
+print(17*13)
+#Hey Bro please dont remove this line
+# day 3
+# print("Hello World") shortcut for comment is ctrl + /
+print("Hello World")
+"""shortcut for multiple line comment is alt + shift + A or ctrl + shift + /  or ctrl + k + c   or ctrl + k + u  or ctrl + k + 0  or ctrl + k + 1   or ctrl + k + 2   or ctrl + k + 3   or ctrl + k + 4   or ctrl + k + 5   or ctrl + k + 6   or          ctrl + k + 7   or ctrl + k + 8    """
+
+print("Hey" , 6 , 7 , sep="~" , end="009\n")
+print("Prince")
