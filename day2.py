e58b616ef352cbc9b9669bc9ff651a148024b0fa
princeyadav7463 <143525918+princeyadav7463@users.noreply.github.com>
2025-06-13 18:22:21 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,12 @@
+#day 2
+
+a=2
+print(a)
+
+b="Prince"
+print(b)
+
+c = True
+print(c)
+a1=20
+print(a+a1)
---
+++ day3.py
@@ -0,0 +1,23 @@
+
+# day = 3
+
+a= complex(8,2)
+b = True
+c = "Prince"
+d = None
+print(a)
+print(b)
+a1 = 9
+print("The type of a is ", type(a))
+print("The type of b is ", type(b))
+print("The type of c is ", type(c))
+print(list)
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))  # tuple is immutabl    e
+print(tuple1)
+
+dict1 = {"name":"Prince", "age":20, "canVote":True}  # dictionary is mutable
+print(dict1)
