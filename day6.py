41d20c1a09b255e35f939e7d954e9f1b94911758
Prince Yadav <143525918+princeyadav7463@users.noreply.github.com>
2025-06-15 21:09:04 +0530

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,32 @@
+# day 6
+
+# type casting or type conversion .
+
+'''Type casting
+The conversion of one data type into the other data type is known as type casting in python or type conversion in python.
+
+pyton supports a wide variety of functions or methods like: int(), float(), str(), ord(), hex(), oct(), tuple(), set(), list(), dict(), etc. for the type casting in python.
+
+# Two types of typecasting
+explicit conversion (explicit type casting) - done via methods.
+implicit conversion (implicit type casting) - automatically done by python.
+'''
+
+a = "1"
+b = "2"
+print(a+b)
+
+print(int(a)+int(b))   # explicit type casting  
+# Implicit type casting
+
+c = 1.9
+d = 8
+print(c+d)
+
+a = input("Enter your name:")
+print("My name is" ,a)
+
+x = input("Enter first number:")
+y = input("Enter second number:")
+print(x+y)  # concatenation 
+print(int(x)+int(y))  # type casting 
