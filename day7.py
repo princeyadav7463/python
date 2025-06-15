71b179627b5f102533bb942773cddd8fd3f1f7ee
Prince Yadav <143525918+princeyadav7463@users.noreply.github.com>
2025-06-15 21:09:48 +0530

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,20 @@
+# day 7
+'''
+name = "Prince"
+frind = "Rahul"
+# apple =  He said,
+ # Hi Prince
+# hey I am good
+"I want to eat an apple"
+# print(apple)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+
+
+print("lets use a for loop \n")
+for character in apple:
+    print(character)
