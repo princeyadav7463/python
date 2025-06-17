1400e583ce1c0c494d0de5d080eddfad3e75686b
Prince Yadav <143525918+princeyadav7463@users.noreply.github.com>
2025-06-17 17:29:47 +0530

    Add files via upload

---
+++ day8.py
@@ -0,0 +1,17 @@
+# day  8
+
+name = "Prince,Yadav"
+print(len(name))
+print(name[0:6])
+
+fruit = "Mango"
+mangolen = len(fruit)
+print(mangolen)
+print(fruit[0:4])
+print(fruit[1:4])
+print(fruit[:5])
+
+
+# /Quick Quiz
+nm = "Prince"
+print(nm[-4:-2])  # -4 is the index of the first character and -2 is the index of the last character
