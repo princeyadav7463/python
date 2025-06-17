b370497ea214430f7a56257ccc084a5006c7d43f
Prince Yadav <143525918+princeyadav7463@users.noreply.github.com>
2025-06-17 17:31:36 +0530

    Add files via upload

---
+++ day11.py
@@ -0,0 +1,11 @@
+# day  11
+
+import time
+timestamp = time.strftime('%H:%M:%S')  # %H is for hour, %M is for minute, %S is for second
+print(timestamp)
+timestamp = time.strftime('%H')
+print(timestamp)
+timestamp = time.strftime('%M')  
+print(timestamp)
+timestamp = time.strftime('%S')  
+print(timestamp)
