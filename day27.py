dba2d28b3ef5de98ad9a33def53b83c1a5e69539
Prince Yadav <143525918+princeyadav7463@users.noreply.github.com>
2025-06-22 20:34:43 +0530

    Add files via upload

---
+++ day27.py
@@ -0,0 +1,14 @@
+# day 27
+# set in python 
+
+s = {2, 4, 2, 6}
+print(s)  # set is unordered and unindexed and it does not allow duplicate values and it is mutable and it is used to store the data which is not going to change.
+
+info = {"Carla", 19, False, 5.9, 19} 
+print(info) 
+
+prince = {}
+print(type(prince))
+
+for value in info:
+  print(value)
