f9aef516a5b79196276574dc2e854d3dc5309e29
Prince Yadav <143525918+princeyadav7463@users.noreply.github.com>
2025-06-25 20:48:33 +0530

    Add files via upload

---
+++ day31.py
@@ -0,0 +1,17 @@
+# day 31
+
+for i in range(6):
+  print(i)
+  if i == 4:
+    break
+else:
+  print("Sorry no i")
+
+i = 0
+while i<7:
+  print(i)
+  i = i + 1
+  if i == 4:
+    break 
+else:
+  print("Sorry no i")  # this will not be printed as the loop is broken
