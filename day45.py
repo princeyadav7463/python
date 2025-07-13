f494ebf6380dd46ce2c6022ebcf6033c2b399159
Prince Yadav <143525918+princeyadav7463@users.noreply.github.com>
2025-07-13 09:11:56 +0530

    Add files via upload

---
+++ day45.py
@@ -0,0 +1,24 @@
+
+# day 45
+
+def hello():
+ print("Hello World")
+
+hello()
+sales1 = 6000
+profit1 = 2000
+ad1 = 1000
+
+sales2 = 6000
+profit2 = 2000  
+ad2 = 1000
+
+sales3 = 6000  
+profit3 = 2000  
+ad3 = 1000
+
+RAilwayForm -->  Class[blueprint  ]
+Prince --> Prince ka infromation
+Rakesh --> Rakesh ka information  
+Raj  --> Raj ka information
+
